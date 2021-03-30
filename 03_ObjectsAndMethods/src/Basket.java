3fc0c8881e8d7709384da3e17ad256f65bb32ff2
Dmitry Parshin <DmParshin@gmail.com>
2021-03-30 10:05:55 +0700

    DZ 3.10.1

--- 03_ObjectsAndMethods/src/Basket.java
+++ 03_ObjectsAndMethods/src/Basket.java
@@ -5,6 +5,8 @@ public class Basket {
     private int totalPrice = 0;
     private int limit;
 
+    private double totalWeight = 0; // Добавляем переменную общего веса
+
     public Basket() {
         increaseCount(1);
         items = "Список товаров:";
@@ -16,10 +18,11 @@ public class Basket {
         this.limit = limit;
     }
 
-    public Basket(String items, int totalPrice) {
+    public Basket(String items, int totalPrice, double totalWeight) {
         this();
         this.items = this.items + items;
         this.totalPrice = totalPrice;
+        this.totalWeight = totalWeight;
     }
 
     public static int getCount() {
@@ -31,10 +34,14 @@ public class Basket {
     }
 
     public void add(String name, int price) {
-        add(name, price, 1);
+        add(name, price, 1, 0);
     }
 
-    public void add(String name, int price, int count) {
+    public void add(String name, int price, double weight){  // Добавляем метод с параметром веса и отсутствием количества
+        add(name, price, 1, weight);
+    }
+
+    public void add(String name, int price, int count, double weight) {
         boolean error = false;
         if (contains(name)) {
             error = true;
@@ -50,17 +57,23 @@ public class Basket {
         }
 
         items = items + "\n" + name + " - " +
-            count + " шт. - " + price;
+            count + " шт. - " + price +  " у.е" + " - " + weight +  " g";
         totalPrice = totalPrice + count * price;
+        totalWeight = totalWeight + count * weight;
     }
 
     public void clear() {
         items = "";
         totalPrice = 0;
+        totalWeight = 0;
     }
 
     public int getTotalPrice() {
         return totalPrice;
+    }
+
+    public double getTotalWeight() {
+        return totalWeight;
     }
 
     public boolean contains(String name) {
@@ -74,5 +87,7 @@ public class Basket {
         } else {
             System.out.println(items);
         }
+        System.out.println("Total Price: " + this.getTotalPrice());
+        System.out.println("Total Weight: " + this.getTotalWeight());
     }
 }
--- 03_ObjectsAndMethods/src/Main.java
+++ 03_ObjectsAndMethods/src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        Basket basket = new Basket();
-        basket.add("Milk", 40);
-        basket.print("Milk");
+        Basket basket1 = new Basket();
+        basket1.add("Milk", 40);
+        basket1.add("Oil", 140, 2,2.5);
+        basket1.add("Salt", 7, 2.5);
+        basket1.print("");
+
+        Basket basket2 = new Basket();
+        basket2.add("Sugar", 80);
+        basket2.add("Bread", 20, 3,5.0);
+        basket2.add("Salt", 7, 1.5);
+        basket2.print("");
     }
 }
