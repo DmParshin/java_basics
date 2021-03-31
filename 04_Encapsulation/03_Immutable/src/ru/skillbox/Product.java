02f3bff4ccc642f1a3269ae8b40c2f0f1cc01fc2
Dmitry Parshin <DmParshin@gmail.com>
2021-03-31 09:20:37 +0700

    DZ 4.7.2

---
+++ 04_Encapsulation/03_Immutable/src/ru/skillbox/Product.java
@@ -0,0 +1,24 @@
+package ru.skillbox;
+
+public class Product {
+    private final String name;
+    public int price;
+    private final String barCode;
+
+    public Product(String name, String barCode) {
+        this.name = name;
+        this.barCode = barCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getBarCode() {
+        return barCode;
+    }
+}
