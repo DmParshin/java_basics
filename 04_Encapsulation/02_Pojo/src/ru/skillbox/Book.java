2b8f098278e264884f884edd02f4584f2c0e9a86
Dmitry Parshin <DmParshin@gmail.com>
2021-03-31 09:05:00 +0700

    DZ 4.7.1

---
+++ 04_Encapsulation/02_Pojo/src/ru/skillbox/Book.java
@@ -0,0 +1,38 @@
+//Создайте иммутабельный класс Book для хранения информации о книге, содержащий поля:
+//
+//        название,
+//        автор,
+//        количество страниц,
+//        номер ISBN.
+
+package ru.skillbox;
+
+public class Book {
+    private final String name;
+    private final String author;
+    private final int pages;
+    private final String isbn;
+
+    public Book(String name, String author, int pages, String isbn) {
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+        this.isbn = isbn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+}
