f401d84dcb6f021f28ef8bdc777b6f8f0b4861a6
Dmitry Parshin <DmParshin@gmail.com>
2021-03-31 17:11:31 +0700

    DZ 4.10

--- 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
+++ 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
@@ -4,12 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         Dimensions dim1 = new Dimensions(1,2,3);
-        dim1.setHeight(131);
+        dim1 = dim1.setHeight(131);
         Order ord1 = new Order(dim1, 10,"ул.Тестовая 15 кв 24",true, "N-123465",  false);
         System.out.println(ord1);
-        dim1.setHeight(9999999);
-        ord1.setMass(999);
-        System.out.println(dim1);
+        ord1 = ord1.setMass(999);
         System.out.println(ord1);
     }
 }
