658c2001bf11efa3ec238b0eb22adf7be7699b36
Dmitry Parshin <DmParshin@gmail.com>
2021-03-31 08:51:05 +0700

    DZ 4.5.2

---
+++ 04_Encapsulation/02_Pojo/src/ru/skillbox/Cat.java
@@ -0,0 +1,31 @@
+package ru.skillbox;
+
+public class Cat {
+    public String name;
+    public int age;
+    public String color;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
