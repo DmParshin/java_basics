b966362852df03ae44050c3e2b90e8d7840b240a
Dmitry Parshin <DmParshin@gmail.com>
2021-04-01 14:01:02 +0700

    DZ 5.10

---
+++ 05_StaticFinal/03_computer/src/ru/skillbox/Computer.java
@@ -0,0 +1,193 @@
+package ru.skillbox;
+
+import javax.annotation.processing.Processor;
+
+public class Computer {
+
+    public final String vendor;
+    public final String name;
+
+    public ProcessorManufacturer processorManufacturer;
+    public int cpuFrequency;
+    public int cpuCores;
+    public double cpuWeight;
+
+    public RamType ramType;
+    public int ramSize;
+    public double ramWeight;
+
+    public HarddiskType harddiskType;
+    public int hddSize;
+    public double hddWeight;
+
+    public DisplayType displayType;
+    public int displayDiagonal;
+    public double displayWeight;
+
+    public KeyboardType keyboardType;
+    public boolean keyboardLight;
+    public double keyboardWeight;
+
+    public double weight;
+
+    public Computer(String name, String vendor){
+        this.name = name;
+        this.vendor = vendor;
+    }
+
+    public ProcessorManufacturer getProcessorManufacturer() {
+        return processorManufacturer;
+    }
+
+    public void setProcessorManufacturer(ProcessorManufacturer processorManufacturer) {
+        this.processorManufacturer = processorManufacturer;
+    }
+
+    public int getCpuFrequency() {
+        return cpuFrequency;
+    }
+
+    public void setCpuFrequency(int cpuFrequency) {
+        this.cpuFrequency = cpuFrequency;
+    }
+
+    public int getCpuCores() {
+        return cpuCores;
+    }
+
+    public void setCpuCores(int cpuCores) {
+        this.cpuCores = cpuCores;
+    }
+
+    public double getCpuWeight() {
+        return cpuWeight;
+    }
+
+    public void setCpuWeight(double cpuWeight) {
+        this.cpuWeight = cpuWeight;
+        weight = weight + cpuWeight;
+    }
+
+    public RamType getRamType() {
+        return ramType;
+    }
+
+    public void setRamType(RamType ramType) {
+        this.ramType = ramType;
+    }
+
+    public int getRamSize() {
+        return ramSize;
+    }
+
+    public void setRamSize(int ramSize) {
+        this.ramSize = ramSize;
+    }
+
+    public double getRamWeight() {
+        return ramWeight;
+    }
+
+    public void setRamWeight(double ramWeight) {
+        this.ramWeight = ramWeight;
+        weight = weight + ramWeight;
+    }
+
+    public HarddiskType getHarddiskType() {
+        return harddiskType;
+    }
+
+    public void setHarddiskType(HarddiskType harddiskType) {
+        this.harddiskType = harddiskType;
+    }
+
+    public int getHddSize() {
+        return hddSize;
+    }
+
+    public void setHddSize(int hddSize) {
+        this.hddSize = hddSize;
+    }
+
+    public double getHddWeight() {
+        return hddWeight;
+    }
+
+    public void setHddWeight(double hddWeight) {
+        this.hddWeight = hddWeight;
+        weight = weight + hddWeight;
+    }
+
+    public DisplayType getDisplayType() {
+        return displayType;
+    }
+
+    public void setDisplayType(DisplayType displayType) {
+        this.displayType = displayType;
+    }
+
+    public int getDisplayDiagonal() {
+        return displayDiagonal;
+    }
+
+    public void setDisplayDiagonal(int displayDiagonal) {
+        this.displayDiagonal = displayDiagonal;
+    }
+
+    public double getDisplayWeight() {
+        return displayWeight;
+    }
+
+    public void setDisplayWeight(double displayWeight) {
+        this.displayWeight = displayWeight;
+        weight = weight + displayWeight;
+    }
+
+    public KeyboardType getKeyboardType() {
+        return keyboardType;
+    }
+
+    public void setKeyboardType(KeyboardType keyboardType) {
+        this.keyboardType = keyboardType;
+    }
+
+    public boolean isKeyboardLight() {
+        return keyboardLight;
+    }
+
+    public void setKeyboardLight(boolean keyboardLight) {
+        this.keyboardLight = keyboardLight;
+    }
+
+    public double getKeyboardWeight() {
+        return keyboardWeight;
+    }
+
+    public void setKeyboardWeight(double keyboardWeight) {
+        this.keyboardWeight = keyboardWeight;
+        weight = weight + keyboardWeight;
+    }
+
+    public String toString(){
+        return "Название - " + name
+                + "\nПроизводитель - " + vendor
+                + "\nЧастота процессора - " + cpuFrequency
+                + "\nКоличество ядер - " + cpuCores
+                + "\nВес процессора - " + cpuWeight
+                + "\nТип RAM - " + ramType
+                + "\nОбъем RAM - " + ramSize
+                + "\nВес RAM - " + ramWeight
+                + "\nТип HDD - " + harddiskType
+                + "\nОбъем HDD - " + hddSize
+                + "\nВес HDD - " + hddWeight
+                + "\nДиагональ экрана - " + displayDiagonal
+                + "\nТип экрана - " + displayType
+                + "\nВес экрана - " + displayWeight
+                + "\nТип клавиатуры - " + keyboardType
+                + "\nПодсветка - " + keyboardLight
+                + "\nВес клавиатуры - " + keyboardWeight
+                + "\nОбщий вес - " + weight
+                + "\n- - - - - - - - - - "
+                ;
+    }
+}
--- 05_StaticFinal/03_computer/src/ru/skillbox/Main.java
+++ 05_StaticFinal/03_computer/src/ru/skillbox/Main.java
@@ -3,5 +3,78 @@ package ru.skillbox;
 public class Main {
 
     public static void main(String[] args) {
+        Computer comp1 = new Computer("Notebook", "Lenovo");
+        Computer comp2 = new Computer("Notebook", "Apple");
+        Computer comp3 = new Computer("Notebook", "Sony");
+
+
+        comp1.setCpuFrequency(6666);
+        comp1.setCpuCores(99);
+        comp1.setProcessorManufacturer(ProcessorManufacturer.AMD);
+        comp1.setCpuWeight(555);
+
+        comp1.setRamType(RamType.DDR3);
+        comp1.setRamSize(8000);
+        comp1.setRamWeight(150);
+
+        comp1.setHarddiskType(HarddiskType.SSD);
+        comp1.setHddSize(16000);
+        comp1.setHddWeight(1500);
+
+        comp1.setDisplayDiagonal(15);
+        comp1.setDisplayType(DisplayType.TN);
+        comp1.setDisplayWeight(1300);
+
+        comp1.setKeyboardType(KeyboardType.MEMBRANE);
+        comp1.setKeyboardLight(false);
+        comp1.setKeyboardWeight(140);
+// ==========================================
+        comp2.setCpuFrequency(6666);
+        comp2.setCpuCores(22);
+        comp2.setProcessorManufacturer(ProcessorManufacturer.AMD);
+        comp2.setCpuWeight(123);
+
+        comp2.setRamType(RamType.DDR4);
+        comp2.setRamSize(4222);
+        comp2.setRamWeight(150);
+
+        comp2.setHarddiskType(HarddiskType.HDD);
+        comp2.setHddSize(14000);
+        comp2.setHddWeight(1100);
+
+        comp2.setDisplayDiagonal(12);
+        comp2.setDisplayType(DisplayType.IPS);
+        comp2.setDisplayWeight(1300);
+
+        comp2.setKeyboardType(KeyboardType.MEMBRANE);
+        comp2.setKeyboardLight(false);
+        comp2.setKeyboardWeight(140);
+
+// ====================================================
+
+        comp3.setCpuFrequency(6166);
+        comp3.setCpuCores(19);
+        comp3.setProcessorManufacturer(ProcessorManufacturer.AMD);
+        comp3.setCpuWeight(515);
+
+        comp3.setRamType(RamType.DDR4);
+        comp3.setRamSize(4000);
+        comp3.setRamWeight(150);
+
+        comp3.setHarddiskType(HarddiskType.HDD);
+        comp3.setHddSize(11000);
+        comp3.setHddWeight(1100);
+
+        comp3.setDisplayDiagonal(15);
+        comp3.setDisplayType(DisplayType.VA);
+        comp3.setDisplayWeight(1100);
+
+        comp3.setKeyboardType(KeyboardType.MEMBRANE);
+        comp3.setKeyboardLight(false);
+        comp3.setKeyboardWeight(110);
+
+        System.out.println(comp1.toString());
+        System.out.println(comp2.toString());
+        System.out.println(comp3.toString());
     }
 }
