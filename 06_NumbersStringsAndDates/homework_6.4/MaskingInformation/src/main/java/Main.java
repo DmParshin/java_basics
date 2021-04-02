01b698270298c878b35bad70a93718f306b22f78
Dmitry Parshin <DmParshin@gmail.com>
2021-04-02 22:20:47 +0700

    DZ 6.21 +

--- 06_NumbersStringsAndDates/homework_6.4/MaskingInformation/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.4/MaskingInformation/src/main/java/Main.java
@@ -1,12 +1,33 @@
+//        Пользователь вводит строку, в некоторой части которой содержится конфиденциальная информация. Ваша задача изменить
+//        строку и скрыть информацию в < >.
+//
+//        Реализуйте метод String searchAndReplaceDiamonds(String text, String placeholder), который будет заменять в
+//        переданной строке String text всё содержимое скобок <> (англ. diamonds) и сами скобки на переданную строку в
+//        String placeholder.
+
 public class Main {
 
     public static void main(String[] args) {
-
+        //searchAndReplaceDiamonds("sd<as>df<sdf>sd", "kuku");
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
-        return text;
+       for (int i = 0; i < text.length(); i++) {
+           if (text.charAt(i) == '<'){
+               for(int j = i; j < text.length(); j++){
+                   if (text.charAt(j) == '>'){
+                           String str = text.substring(i, j + 1);
+                           text = text.replace(str, placeholder);
+                           i = j + 1;
+                   }
+                   else if ((text.charAt(j) == '<')){
+                       i = j;
+                   }
+               }
+           }
+       }
+       //System.out.println("New string: " + text);
+       return text;
     }
 
 }
