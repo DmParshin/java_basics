be1dca8ae82daa21db618e8dedc42d13a0e4bceb
Dmitry Parshin <DmParshin@gmail.com>
2021-04-02 16:43:19 +0700

    DZ 6.21 - 2

--- 06_NumbersStringsAndDates/homework_6.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,9 +1,30 @@
+//        Напишите код, который считает сумму заработка всех друзей.
+//
+//        Используйте методы indexOf(), lastIndexOf(), substring() и trim().
+//
+//        Использование регулярных выражений в данном задании не допускается.
+
 public class Main {
 
   public static void main(String[] args) {
 
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
+    int startWord = 0;
+    int endWord;
+    int count = 0;
+    int sum = 0;
+    for (int i = 0; i < text.length(); i++){
+      if (text.charAt(i) == ' ') {
+        endWord = i;
+        count++;
+        String str = text.substring(startWord, endWord);
+        startWord = i + 1;
+        if (count == 3 || count == 7 || count == 12){
+          sum = sum + (Integer.parseInt(str));
+          startWord = i + 1;
+        }
+      }
+    }
+    System.out.println(sum);
   }
-
 }
