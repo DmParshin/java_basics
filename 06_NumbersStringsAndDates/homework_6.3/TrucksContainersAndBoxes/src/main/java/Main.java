3b29c5d991a2d5fc5bb06db0ac4be10641b61f1d
Dmitry Parshin <DmParshin@gmail.com>
2021-04-02 15:20:41 +0700

    DZ 6.17+

--- 06_NumbersStringsAndDates/homework_6.3/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.3/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.Scanner;
 
 public class Main {
@@ -5,20 +6,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String boxes = scanner.nextLine();
+        int numberOfBoxes = Integer.parseInt(boxes) ;
+        int x;
+        int y;
+        int trucksCount = 0;
+        int containerCount = 0;
+        int boxesCount = 0;
+        if (numberOfBoxes > 0) {
+            do {
+                trucksCount++;
+                System.out.println("Грузовик: " + trucksCount);
+                x = 1;
+                while((numberOfBoxes - (containerCount * 27) > 0) && x <= 12) {
+                    System.out.println("\tКонтейнер: " + x);
+                    containerCount++;
+                    x++;
+                    y = 1;
+                    while ((numberOfBoxes > boxesCount) && y <= 27){
+                        System.out.println("\t\tЯщик: " + y);
+                        boxesCount++;
+                        y++;
+                    }
+                }
+            }
+            while (trucksCount * 324 < numberOfBoxes);
 
-        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
-        // пример вывода при вводе 2
-        // для отступа используйте табуляцию - \t
-
-        /*
-        Грузовик: 1
-            Контейнер: 1
-                Ящик: 1
-                Ящик: 2
-        Необходимо:
-        грузовиков - 1 шт.
-        контейнеров - 1 шт.
-        */
+            System.out.println("Необходимо:\nгрузовиков - " + trucksCount + " шт."+ "\nконтейнеров - " + containerCount + " шт.");
+        }
     }
 
 }
