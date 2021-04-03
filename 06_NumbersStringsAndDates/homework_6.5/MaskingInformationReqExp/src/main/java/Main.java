1671d6ae773c8b5feaa40b6f405ffde2d4a6272b
Dmitry Parshin <DmParshin@gmail.com>
2021-04-03 15:30:13 +0700

    DZ 6.25.+

--- 06_NumbersStringsAndDates/homework_6.5/MaskingInformationReqExp/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.5/MaskingInformationReqExp/src/main/java/Main.java
@@ -1,12 +1,18 @@
+//        Реализовать метод
+//
+//        String searchAndReplaceDiamonds(String text, String placeholder),
+//        который будет заменять в переданной строке String text всё содержимое скобок <> и сами
+//        скобки на переданную строку в String placeholder, используя регулярные выражения. Учтите вариант,
+//        когда необходимо заменить несколько замен <> в переданной строке.
+
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(searchAndReplaceDiamonds("<4008> 1234 <5678> 8912", "****"));
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+            text = text.replaceAll("<.{1,}?>", placeholder);
         return text;
     }
-
 }
