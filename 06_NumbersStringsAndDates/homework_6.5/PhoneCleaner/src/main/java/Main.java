1f7fb453bf3c1bf707aec892dee260080ebf43ef
Dmitry Parshin <DmParshin@gmail.com>
2021-04-03 14:57:50 +0700

    DZ 6.25.4

--- 06_NumbersStringsAndDates/homework_6.5/PhoneCleaner/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.5/PhoneCleaner/src/main/java/Main.java
@@ -1,3 +1,7 @@
+//        Реализуйте удаление лишних символов при вводе номера телефона в консоли и проверку соответствия номера формату
+//        мобильных номеров России. Если введённую строку нельзя привести к формату мобильного номера — выводите сообщение
+//        о неверном вводе. Телефон может быть введён не только в формате 79091234567, но и с лишними символами.
+
 import java.util.Scanner;
 
 public class Main {
@@ -10,7 +14,30 @@ public class Main {
       if (input.equals("0")) {
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+
+      input = input.replaceAll("[\\s+.,;()+-]", "");
+
+      String[] stringArray = input.split("");
+
+      if (stringArray.length == 11 && Integer.parseInt(stringArray[0]) == 7){
+      }
+      else if (stringArray.length == 11 && Integer.parseInt(stringArray[0]) == 8) {
+        stringArray[0] = "7";
+      }
+      else if (stringArray.length == 10 && Integer.parseInt(stringArray[0]) == 9){
+        String[] tempArray= new String[stringArray.length + 1];
+        tempArray[0] = "7";
+        for (int i = 0; i < stringArray.length; i++){
+          tempArray[i + 1] = stringArray[i];
+        }
+        stringArray = tempArray;
+      }
+      else {
+        System.out.println("Неверный формат номера");
+        break;
+      }
+    String str = String.join("", stringArray);
+    System.out.println(str);
     }
   }
 
