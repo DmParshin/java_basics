c98d16a897a7704c0cf4e52f7b62718349378c52
Dmitry Parshin <DmParshin@gmail.com>
2021-04-03 12:36:51 +0700

    DZ 6.25.2

--- 06_NumbersStringsAndDates/homework_6.5/SplitText/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.5/SplitText/src/main/java/Main.java
@@ -1,12 +1,19 @@
+//        Реализовать метод, который принимает в качестве аргумента строку, содержащую английский текст (не менее 100 слов).
+//        Метод должен разделить текст на слова и вернуть строку, состоящую из полученных слов, каждое слово на отдельной строке.
+
+import java.awt.*;
+
 public class Main {
 
   public static void main(String[] args) {
-
+    splitTextIntoWords("Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей");
   }
 
   public static String splitTextIntoWords(String text) {
-    //TODO реализуйте метод
-    return "";
+    text = text.replaceAll("[\n\\s+.,;-[0-9]]", " ");
+    text = text.trim().replaceAll("\\s+\\B", "");
+    text = text.replaceAll("[\\s]", "\n");
+    return text;
   }
 
 }
