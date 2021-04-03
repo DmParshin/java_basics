036b33c8de3ea6ab001e97917d28f6e70c19e6c5
Dmitry Parshin <DmParshin@gmail.com>
2021-04-03 18:32:20 +0700

    DZ 6.27

--- 06_NumbersStringsAndDates/homework_6.6/BirthdayCalendar/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.6/BirthdayCalendar/src/main/java/Main.java
@@ -1,29 +1,37 @@
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int day = 18;
-        int month = 8;
-        int year = 1987;
+        int day = 31;
+        int month = 12;
+        int year = 2010;
 
         System.out.println(collectBirthdays(year, month, day));
 
     }
 
     public static String collectBirthdays(int year, int month, int day) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy - EEEE");
-        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy - EE",Locale.ENGLISH);
+
+        Calendar todayDate = Calendar.getInstance();
+
+        Calendar setCalendar = Calendar.getInstance();
+        setCalendar.set(Calendar.YEAR, year);
+        setCalendar.set(Calendar.DAY_OF_MONTH, day);
+        setCalendar.set(Calendar.MONTH, month - 1);
         int d = 0;
-        for (int i = 1987; i < 2021; i++){
-            calendar.set(Calendar.YEAR, i);
-            calendar.set(Calendar.DAY_OF_MONTH, day);
-            calendar.set(Calendar.MONTH, month - 1);
-            System.out.println(d + " - " + dateFormat.format(calendar.getTime()));
+        String str = "";
+        while (setCalendar.before(todayDate) || setCalendar.equals(todayDate)) {
+            str = str + (d + " - " + dateFormat.format(setCalendar.getTime()) + System.lineSeparator());
+            setCalendar.set(Calendar.YEAR, year + d + 1);
+            setCalendar.set(Calendar.DAY_OF_MONTH, day);
+            setCalendar.set(Calendar.MONTH, month - 1);
             d++;
         }
-        return "";
+        return str;
     }
 }
