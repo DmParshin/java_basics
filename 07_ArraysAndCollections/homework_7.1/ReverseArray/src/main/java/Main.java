a61dbac326c1c12889c62f3722502e1e539dbee0
Dmitry Parshin <DmParshin@gmail.com>
2021-04-05 10:31:39 +0700

    DZ 7.5.1

--- 07_ArraysAndCollections/homework_7.1/ReverseArray/src/main/java/Main.java
+++ 07_ArraysAndCollections/homework_7.1/ReverseArray/src/main/java/Main.java
@@ -1,6 +1,11 @@
+import javax.naming.PartialResultException;
+
 public class Main {
 
     public static void main(String[] args) {
         String line = "Каждый охотник желает знать, где сидит фазан";
+        String[] strings = line.split(",?\\s+");
+        String[] newArray =  ReverseArray.reverse(strings);
+        for(String i : newArray){ System.out.println(i); }
     }
 }
--- 07_ArraysAndCollections/homework_7.1/ReverseArray/src/main/java/ReverseArray.java
+++ 07_ArraysAndCollections/homework_7.1/ReverseArray/src/main/java/ReverseArray.java
@@ -1,7 +1,16 @@
 public class ReverseArray {
 
     //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
-    public static String[] reverse (String[] strings){
+    public static String[] reverse (String[] strings) {
+        String str;
+        for (int i = 0; i < strings.length / 2; i++) {
+
+            str = strings[(strings.length - 1) - i];
+
+            strings[(strings.length - 1) - i] = strings[i];
+
+            strings[i] = str;
+        }
         return strings;
     }
 }
