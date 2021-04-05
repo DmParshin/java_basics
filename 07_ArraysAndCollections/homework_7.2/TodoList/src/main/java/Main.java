1bd720ff5ec85b112d4942fc00844d0e2a760c21
Dmitry Parshin <DmParshin@gmail.com>
2021-04-05 16:26:32 +0700

    DZ 7.7

--- 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/Main.java
+++ 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/Main.java
@@ -1,7 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
     private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
         // TODO: написать консольное приложение для работы со списком дел todoList
+        Scanner in = new Scanner(System.in);
+        String task;
+        String task1;
+        Integer number;
+
+        while(true){
+            System.out.println("Введите команду: ");
+            String command = in.nextLine();
+            switch (command){
+                case "ADD":
+                    System.out.println("Введите дело");
+                    task = in.nextLine();
+                    System.out.println("Введите номер в списке");
+                    number = in.nextInt();
+                    todoList.add(number, task);
+                    break;
+                case "EDIT":
+                    System.out.println("Введите дело");
+                    task = in.nextLine();
+                    System.out.println("Введите номер в списке");
+                    number = in.nextInt();
+                    todoList.edit(task, number);
+                    break;
+                case "DELETE":
+                    System.out.println("Введите номер в списке");
+                    number = in.nextInt();
+                    todoList.delete(number);
+                    break;
+                case "LIST":
+                    todoList.getTodos();
+                    break;
+                case "EXIT":
+                    return;
+            }
+        }
+//        return;
     }
 }
+
--- 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/TodoList.java
+++ 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/TodoList.java
@@ -2,28 +2,43 @@ import java.util.ArrayList;
 
 public class TodoList {
 
+    ArrayList<String> todoList = new ArrayList<>();
+
+    public TodoList() {
+        ArrayList<String> todoList = new ArrayList<>();
+    }
+
     public void add(String todo) {
         // TODO: добавьте переданное дело в конец списка
+        this.todoList.add(todo);
     }
 
     public void add(int index, String todo) {
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
+        if (index <= todoList.size()){ this.todoList.add(index, todo); }
+        else { this.todoList.add(todo);}
     }
 
     public void edit(String todo, int index) {
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
+        if (index < todoList.size()){this.todoList.set(index, todo);}
+
     }
 
     public void delete(int index) {
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
+        if (index < todoList.size()){this.todoList.remove(index);}
     }
 
     public ArrayList<String> getTodos() {
         // TODO: вернуть список дел
-        return new ArrayList<>();
+        for (int i = 0; i < todoList.size(); i++){
+            System.out.println(i + " - " + todoList.get(i));
+        }
+        return this.todoList;
     }
 
 }
