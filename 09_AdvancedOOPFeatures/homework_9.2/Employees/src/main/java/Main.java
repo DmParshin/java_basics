9bb090b6e2d731276c7731c618555a8db2435eff
Dmitry Parshin <DmParshin@gmail.com>
2021-04-08 17:21:29 +0700

    DZ 9.8

--- 09_AdvancedOOPFeatures/homework_9.2/Employees/src/main/java/Main.java
+++ 09_AdvancedOOPFeatures/homework_9.2/Employees/src/main/java/Main.java
@@ -1,3 +1,5 @@
+
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -7,12 +9,19 @@ public class Main {
     public static void main(String[] args) {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
         Employee employeeMaxSalary = findEmployeeWithHighestSalary(staff, 2017);
-        System.out.println(employeeMaxSalary);
+//        System.out.println(employeeMaxSalary);
     }
 
     public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
         //TODO Метод должен вернуть сотрудника с максимальной зарплатой среди тех,
         // кто пришёл в году, указанном в переменной year
+        Date setDate = new Date(year - 1900,01,01);
+//        System.out.println(setDate);
+        staff.stream()
+                .map(employee -> employee)
+                .filter(employee -> employee.getWorkStart().after(setDate))
+                .max((o1, o2) -> o1.getSalary())
+                .ifPresent(System.out::println);
         return null;
     }
 }
