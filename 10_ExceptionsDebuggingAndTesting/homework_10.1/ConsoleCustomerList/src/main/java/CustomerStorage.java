1da8aed9af72c068c84680855620ff5c0997a030
Dmitry Parshin <DmParshin@gmail.com>
2021-04-12 11:11:51 +0700

    DZ 10.4

--- 10_ExceptionsDebuggingAndTesting/homework_10.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ 10_ExceptionsDebuggingAndTesting/homework_10.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class CustomerStorage {
     private final Map<String, Customer> storage;
@@ -14,16 +15,32 @@ public class CustomerStorage {
         final int INDEX_EMAIL = 2;
         final int INDEX_PHONE = 3;
 
+        final String patternEmail = "^[-\\w.]+@([A-z0-9][-A-z0-9]+\\.)+[A-z]{2,4}$";
+        final String patternPhone = "^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{10}$";
+
         String[] components = data.split("\\s+");
+
+        if(components.length!=4) {
+            throw new IllegalArgumentException("Wrong format. Correct format - add Василий Петров vasily.petrov@gmail.com +79215637722");}
+
+        else if (!Pattern.matches(patternEmail,components[INDEX_EMAIL])) {
+            throw new IllegalArgumentException("Wrong Email format. Correct format - vasily.petrov@gmail.com");}
+
+        else if (!Pattern.matches(patternPhone,components[INDEX_PHONE])) {
+            throw new IllegalArgumentException("Wrong phone number format. Correct format - +79215637722");}
+
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
-    }
+
+        }
 
     public void listCustomers() {
         storage.values().forEach(System.out::println);
     }
 
     public void removeCustomer(String name) {
+        if(!storage.containsKey(name)) {
+            throw new IllegalArgumentException("Wrong name. Name is not found");}
         storage.remove(name);
     }
 
