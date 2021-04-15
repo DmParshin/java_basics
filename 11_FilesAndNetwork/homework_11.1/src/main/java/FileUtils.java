5b8eb1876e37335ae850bee760fd7ffc3da1e1a2
Dmitry Parshin <DmParshin@gmail.com>
2021-04-15 15:44:21 +0700

    DZ 11.5

--- 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
@@ -1,23 +1,29 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        try {
             long size = org.apache.commons.io.FileUtils.sizeOfDirectory(new File(path));
-
-            System.out.println("Folder Size: " + org.apache.commons.io.FileUtils.byteCountToDisplaySize(size));
-
             return (size);
-        }
-        catch (IllegalArgumentException e){
-            System.out.println("Нет такой папки!");
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return 0;
     }
 
+    public static long calculateFolderSizeWalk(String path) throws IOException {
+
+            long dirSize = Files.walk(Path.of(path))
+                    .map(Path::toFile)
+                    .filter(File::isFile)
+                    .mapToLong(File::length)
+                    .sum();
+            return dirSize;
+    }
+
+    public static String formatSize(long v) {
+        if (v < 1024) return v + " B";
+        int z = (63 - Long.numberOfLeadingZeros(v)) / 10;
+        return String.format("%.1f %sB", (double)v / (1L << (z*10)), " KMGTPE".charAt(z));
+    }
 }
+
--- 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
@@ -1,12 +1,20 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
         while (true) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Введите путь до папки:");
-            String path = scanner.nextLine();
-            FileUtils.calculateFolderSize(path);
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Введите путь до папки:");
+                String path = scanner.nextLine();
+                System.out.println("Folder Size (org.apache.commons.io): " + org.apache.commons.io.FileUtils.byteCountToDisplaySize(FileUtils.calculateFolderSize(path)));
+                System.out.println("Folder Size (Files.walk stream): " + FileUtils.formatSize(FileUtils.calculateFolderSizeWalk(path)));
+            }
+            catch (Exception e){
+                System.out.println("Нет такой папки или папка недоступна");
+            }
         }
     }
 }
