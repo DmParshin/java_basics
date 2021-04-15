f71d25d9377cec868a45e402957ba313cc5a7778
Dmitry Parshin <DmParshin@gmail.com>
2021-04-15 10:41:02 +0700

    DZ 11.5

--- 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
@@ -1,6 +1,23 @@
+import java.io.File;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
+        try {
+            long size = org.apache.commons.io.FileUtils.sizeOfDirectory(new File(path));
+
+            System.out.println("Folder Size: " + org.apache.commons.io.FileUtils.byteCountToDisplaySize(size));
+
+            return (size);
+        }
+        catch (IllegalArgumentException e){
+            System.out.println("Нет такой папки!");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
         return 0;
     }
+
 }
--- 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите путь до папки:");
+            String path = scanner.nextLine();
+            FileUtils.calculateFolderSize(path);
+        }
     }
 }
