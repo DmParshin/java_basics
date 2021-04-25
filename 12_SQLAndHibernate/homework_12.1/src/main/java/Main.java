8c9f9c525f201a44c506b3d876465d2287257141
Dmitry Parshin <DmParshin@gmail.com>
2021-04-25 12:16:14 +0700

    DZ 12.1

--- 12_SQLAndHibernate/homework_12.1/src/main/java/Main.java
+++ 12_SQLAndHibernate/homework_12.1/src/main/java/Main.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 
 public class Main {
@@ -10,39 +7,35 @@ public class Main {
         String user = "root";
         String pass = "789654123";
 
-        try {
-            Connection connection = DriverManager.getConnection(url, user, pass);
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM Courses ORDER BY name;");
+        try (Connection connection = DriverManager.getConnection(url, user, pass);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM Courses ORDER BY name;");)
+        {
             while (resultSet.next()){
                 String courseName = resultSet.getString("name");
                 System.out.println(courseName);
-
-                Statement statement1 = connection.createStatement();
-                int countMonths = 0;
-                int lastMonth = 0;
-                for(int i = 1; i <= 12; i++) {
-                    ResultSet resultSet1 = statement1.executeQuery("SELECT pl.course_name, pl.subscription_date FROM PurchaseList pl WHERE pl.course_name = \"" + courseName + "\" and MONTH(pl.subscription_date ) =" + i + ";");
-                    if (resultSet1.next()) {
-//                        String string = resultSet1.getString("subscription_date");
-//                        System.out.println("\t" + string);
-                        countMonths++;
-                        lastMonth = i;
+                String SQL = "SELECT pl.course_name, pl.subscription_date FROM PurchaseList pl WHERE pl.course_name = ? " +
+                        "and MONTH(pl.subscription_date) = ?";
+                try (PreparedStatement preparedStatement = connection.prepareStatement(SQL);){
+                    int countMonths = 0;
+                    int lastMonth = 0;
+                    for(int i = 1; i <= 12; i++) {
+                        preparedStatement.setString(1, courseName);
+                        preparedStatement.setInt(2, i);
+                        try(ResultSet resultSet1 = preparedStatement.executeQuery();) {
+                            if (resultSet1.next()) {
+                                countMonths++;
+                                lastMonth = i;
+                            }
+                        }
                     }
-                    resultSet1.close();
+                    if(countMonths > 0 && lastMonth > 0){
+                        double avg =  (double)countMonths / (double)lastMonth;
+                        System.out.printf("\tСреднее количество покупок в месяц для курса: %.2f %n", avg);
+                    }else
+                        System.out.println("\tПокупок не было");
                 }
-                statement1.close();
-//                System.out.println("\tКоличество месяцев " + countMonths);
-//                System.out.println("\tПоследний месяц " + lastMonth);
-                if(countMonths > 0 && lastMonth > 0){
-                    double avg =  (double)countMonths / (double)lastMonth;
-                    System.out.printf("\tСреднее количество покупок в месяц для курса: %.2f %n", avg);
-                }else
-                    System.out.println("\tПокупок не было");
             }
-            resultSet.close();
-            statement.close();
-            connection.close();
         }
         catch (Exception e){
             e.printStackTrace();
