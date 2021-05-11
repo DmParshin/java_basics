0b44986e7a8e26bf905980132ddf7a633e7e9faa
Dmitry Parshin <DmParshin@gmail.com>
2021-05-11 20:45:59 +0700

    DZ 14.2

--- 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/Storage.java
+++ 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/Storage.java
@@ -10,7 +10,7 @@ public class Storage {
     private static int currentId = 1;
     private static HashMap<Integer, Deal> deals = new HashMap<>();
 
-    public static int addDeal(Deal deal){
+    public static synchronized int addDeal(Deal deal){
         int id = currentId++;
         deal.setId(id);
         deal.setDone(false);
@@ -18,32 +18,32 @@ public class Storage {
         return id;
     }
 
-    public static Deal getDeal (int dealId){
+    public static synchronized Deal getDeal (int dealId){
         if(deals.containsKey(dealId)){
             return deals.get(dealId);
         }
         return null;
     }
 
-    public static List<Deal> getAllDeals(){
+    public static synchronized List<Deal> getAllDeals(){
         ArrayList<Deal> dealList = new ArrayList<Deal>();
         dealList.addAll(deals.values());
         return dealList;
     }
 
-    public static void delDeal (int dealId){
+    public static synchronized void delDeal (int dealId){
         if(deals.containsKey(dealId)){
             deals.remove(dealId);
         }
     }
 
-    public static void delAllDeal (){
+    public static synchronized void delAllDeal (){
         if(deals.size() != 0){
             deals.clear();
         }
     }
 
-    public static void updateDeal (int dealId, int number, String name, boolean done){
+    public static synchronized void updateDeal (int dealId, int number, String name, boolean done){
         if(deals.containsKey(dealId)){
             Deal deal = getDeal(dealId);
             deal.setNumber(number);
@@ -52,7 +52,7 @@ public class Storage {
         }
     }
 
-    public static void updateAllDeal (boolean done){
+    public static synchronized void updateAllDeal (boolean done){
         if(deals.size() != 0){
             deals.values().forEach(d -> d.setDone(done));
         }
