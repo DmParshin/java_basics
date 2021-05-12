774ed4a7639d429554a9a754ee373e59c8d7ef7b
Dmitry Parshin <DmParshin@gmail.com>
2021-05-12 20:30:26 +0700

    DZ 14.4

--- 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
+++ 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
@@ -1,21 +1,31 @@
 package main;
 
+import main.model.Deal;
+import main.model.DealRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Date;
 
-@RestController
-
+@Controller
 public class DefaultController {
+
+    @Autowired
+    DealRepository dealRepository;
+
     @RequestMapping("/")
-    public ResponseEntity<Date> getCountry() {
+    public String index(Model model) {
+        Iterable<Deal>dealIterable = dealRepository.findAll();
+        ArrayList<Deal>dealArrayList = new ArrayList<>();
+        dealIterable.forEach(deal -> dealArrayList.add(deal));
+        model.addAttribute("deals", dealArrayList);
 
-        var headers = new HttpHeaders();
-        headers.add("Responded", "DefaultController");
-
-        return ResponseEntity.ok(new Date());
+        return "index";
     }
 }
