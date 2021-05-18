5eaff114f8a7ccbbb4b5f343ff827caa1fdfa583
Dmitry Parshin <DmParshin@gmail.com>
2021-05-18 14:28:57 +0700

    DZ 15.4

--- 15_NoSQL/homework_15.4/src/main/java/Main.java
+++ 15_NoSQL/homework_15.4/src/main/java/Main.java
@@ -3,9 +3,11 @@ import com.mongodb.client.*;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
-
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
@@ -42,29 +44,51 @@ public class Main {
                 Document tempDocument = Document.parse(String.format("{name : '%s'}, {$addToSet: {products: '$s'}}", command[1], command[2]));
                 collectionShops.insertOne(tempDocument);
 
+                if (collectionProducts.find(new Document(command[1], new Document("$eq", command[2]))).first() == null ||
+                        collectionShops.find(new Document(command[2], new Document("$eq", command[1]))).first() == null) {
+                    System.out.println("Shopname or Productname not found");
+                    return;
+                }
+
+                Document document = new Document(command[2], new Document("$eq", command[1]));
+
+                Double priceToAdd = (Double) collectionProducts.find(new Document(command[1], new Document("$eq", command[2]))).first().get("price");
+
+                Document document1 = new Document("$addToSet", new Document("$product_list",
+                        new Document(command[1], command[2]).append("price", priceToAdd)));
+
+                collectionShops.updateOne(document, document1);
+
             }else if (command.length == 1 && command[0].matches("statistic")){
 
-                AggregateIterable<Document> documents = collectionShops.aggregate(Arrays.asList(
-                        Document.parse(
-                                "{ $lookup: { " +
-                                        "from: 'products', " +
-                                        "localField: 'products', " +
-                                        "foreignField: 'name', " +
-                                        "as: 'product_list' } }, " +
-                                "{ $unwind: {" +
-                                        "path: '$product_list'} }," +
-                                "{ $group: {" +
-                                        "_id: '$name'," +
-                                        "count_products: { $sum: 1}," +
-                                        "avgprice: {$avg: $product_list.price}," +
-                                        "maxprice: {$max: $product_list.price}," +
-                                        "minprice: {$min: $product_list.price}," +
-                                        "'count_products < 100': { $sum: {$cond: [{ $lt: [ $product_list.price, 100]}, 1, 0]}}}"
-                        )));
+                Bson lookup = new Document("$lookup",
+                        new Document("from", "products")
+                                .append("localField", "products")
+                                .append("foreignField", "name")
+                                .append("as", "product_list"));
 
+                Bson unwind = new Document("$unwind",
+                        new Document("path", "$product_list"));
+
+                Bson group = new Document("$group",
+                        new Document ("_id", "$name")
+                        .append("count_products", new Document ("$sum", 1))
+                        .append("avgprice", new Document("$avg", "$product_list.price"))
+                        .append("maxprice", new Document("$max", "$product_list.price"))
+                        .append("minprice", new Document("$min", "$product_list.price"))
+                        .append("count_products < 100", new Document("$sum", new Document("$cond", Arrays.asList(new Document("$lt", Arrays.asList("$product_list.price", 100)), 1, 0 ))))
+                );
+
+                List<Bson> filters = new ArrayList<>();
+                filters.add(lookup);
+                filters.add(unwind);
+                filters.add(group);
+
+                AggregateIterable<Document> documents = collectionShops.aggregate(filters);
+
+                System.out.println("STATISTIC:");
                 for (Document document : documents) {
                     System.out.println(document.toJson());
-                    System.out.println("Total products: " + document.getInteger("count_products"));
                 }
             }else System.out.println("WRONG COMMAND");
         }
